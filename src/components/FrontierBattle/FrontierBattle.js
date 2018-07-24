d335af6e0ac2641cf15d025620baf73b001fbfb7
Jonathan <jleack@gmail.com>
2018-07-23 17:20:26 -0700

    Added animations and new map

--- src/components/FrontierBattle/FrontierBattle.js
+++ src/components/FrontierBattle/FrontierBattle.js
@@ -1,6 +1,18 @@
 import React, { Fragment } from "react";
+import { bounceIn } from 'react-animations';
+import { StyleSheet, css } from 'aphrodite';
 
 import CharacterPartyCard from '../CharacterPartyCard/CharacterPartyCard';
+
+/*======================================================== 
+// Animated styles using Aphrodite and React Animations.
+========================================================*/
+const styles = StyleSheet.create({
+    bounceIn: {
+        animationName: bounceIn,
+        animationDuration: '1s'
+    }
+})
 
 const FrontierBattle = (props) => {
     return (
@@ -21,15 +33,15 @@ const FrontierBattle = (props) => {
             {(props.enemyAvatar && props.enemyData)
             ? <Fragment>
                 <div className="frontier-enemy-menu">
-                <div className="enemy-character">
-                    <img src={props.enemyAvatar} className="enemy-character-image" alt="Redux Saga Frontier enemy avatar" />
-                    <h3 className="enemy-character-data">{props.enemyData.name.last}</h3> 
-                    <h3 className="enemy-character-data">Level {props.enemyLevel}</h3>   
-                </div>
+                    <div className={["enemy-character", css(styles.bounceIn)].join(' ')}>
+                        <img src={props.enemyAvatar} className="enemy-character-image" alt="Redux Saga Frontier enemy avatar" />
+                        <h3 className="enemy-character-data">{props.enemyData.name.last}</h3> 
+                        <h3 className="enemy-character-data">Level {props.enemyLevel}</h3>   
+                    </div>
                 </div>
                 <div className="button-menu">
-                <button className="secondary-button">Retreat</button>
-                <button className="red-button" onClick={props.onBattle}>Battle Enemy</button>
+                    <button className="secondary-button">Retreat</button>
+                    <button className="red-button" onClick={props.onBattle}>Battle Enemy</button>
                 </div>
             </Fragment>
             : <div className="score-menu">
--- src/components/GameOver/GameOver.js
+++ src/components/GameOver/GameOver.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { fadeIn } from 'react-animations';
+import { StyleSheet, css } from 'aphrodite';
 
 import './GameOver.css';
 
+/*======================================================== 
+// Animated styles using Aphrodite and React Animations.
+========================================================*/
+const styles = StyleSheet.create({
+    fadeIn: {
+        animationName: fadeIn,
+        animationDuration: '2s'
+    }
+})
+
 const GameOver = (props) => {
     return (
-        <div className="game-over">
+        <div className={["game-over", css(styles.fadeIn)].join(' ')}>
             <div className="game-over-text">
                 <h1>GAME OVER</h1>
                 <h1>Final Score: {props.score}</h1>
